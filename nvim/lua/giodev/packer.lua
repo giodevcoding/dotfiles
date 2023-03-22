824fe2ff15ca55bace6842e681ffd3b4e07cca61
Giovanni Panzetta <giodevcoding@gmail.com>
2023-03-22 16:18:10 -0500

    Markdown-preview fix

--- nvim/lua/giodev/packer.lua
+++ nvim/lua/giodev/packer.lua
@@ -124,11 +124,5 @@ return require('packer').startup(function()
     }
 
     -- markdown-preview
-    -- install without yarn or npm
-    use({
-        "iamcco/markdown-preview.nvim",
-        run = function() vim.fn["mkdp#util#install"]() end,
-    })
-
     use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 end)
