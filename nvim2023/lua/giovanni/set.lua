d89c11f06f39fd046704c90efb3c281c1b20aeae
Giovanni Panzetta <giodevcoding@gmail.com>
2023-04-17 16:54:09 -0500

    adjusted settings

--- nvim2023/lua/giovanni/set.lua
+++ nvim2023/lua/giovanni/set.lua
@@ -8,11 +8,15 @@ vim.opt.expandtab = true
 
 vim.api.nvim_create_autocmd("FileType", { pattern = {"javascript", "javascriptreact", "vue", "html", "typescriptreact", "typescript"}, command = [[ setlocal tabstop=2 softtabstop=2 shiftwidth=2 ]] })
 
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 
-vim.opt.hlsearch = true
+vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.background = "dark"
