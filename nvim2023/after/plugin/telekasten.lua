bfea2484f6a0e83f3a0326e0eebd353a708ab76f
Giovanni P <giodevcoding@gmail.com>
2023-09-14 10:41:29 -0500

    disable telekasten for windows

--- nvim2023/after/plugin/telekasten.lua
+++ nvim2023/after/plugin/telekasten.lua
@@ -2,12 +2,27 @@ local telekasten = require("telekasten")
 
 local home = vim.fn.expand("~/zk")
 
-telekasten.setup({
-    home = home,
-    dailies = home .. "/journal/daily",
-    weeklies = home .. "/journal/weekly",
+local function exists(file)
+    local ok, err, code = os.rename(file, file)
+    if not ok then
+        if code == 13 then
+            return true
+        end
+    end
+    return ok, err
+end
 
-    template_new_note = home .. '/templates/new_note.md',
-    template_new_daily = home .. '/templates/daily.md',
-    template_new_weekly= home .. '/templates/weekly.md',
-})
+
+--Windows
+if not (exists("C:\\Windows")) then
+    telekasten.setup({
+        home = home,
+        dailies = home .. "/journal/daily",
+        weeklies = home .. "/journal/weekly",
+
+        template_new_note = home .. '/templates/new_note.md',
+        template_new_daily = home .. '/templates/daily.md',
+        template_new_weekly= home .. '/templates/weekly.md',
+    })
+end
+
