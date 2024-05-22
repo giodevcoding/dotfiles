9c689d602938e441bb25e7341a45fc014777a016
Giovanni Panzetta <giovanni.panzetta@daveramsey.com>
2024-05-22 18:17:45 -0500

    remove need for bg file

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -21,8 +21,10 @@ M = {
     background = {
         {
             source = {
-                File = (wezterm.home_dir .. "/.config/wezterm/bgcolor.png")
+                Color = '#222'
             },
+            width = "100%",
+            height = "100%",
             opacity = 1,
         },
         {
