c6c2eb14393162413525e992f83042d9eafdf4e2
Giovanni Panzetta <giovanni.panzetta@daveramsey.com>
2025-02-17 17:17:53 -0600

    latest

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,16 @@ local wezterm = require 'wezterm'
 
 math.randomseed(os.time())
 
-bg_img_num = math.random(1, 11)
+local bg_img_num = math.random(1, 6)
+
+local use_changing_bg = false
+
+local background_file = (wezterm.home_dir .. "/.config/wezterm/background.png")
+
+if use_changing_bg then
+    background_file = (wezterm.home_dir .. "/.config/wezterm/wezbgs/" .. bg_img_num .. ".png")
+end
+
 
 M = {
     --    default_prog = { 'wsl' },
@@ -21,7 +30,7 @@ M = {
     background = {
         {
             source = {
-                Color = '#222'
+                Color = '#245'
             },
             width = "100%",
             height = "100%",
@@ -29,9 +38,9 @@ M = {
         },
         {
             source = {
-                File = (wezterm.home_dir .. "/.config/wezterm/wezbgs/" .. bg_img_num .. ".png")
+                File = background_file
             },
-            opacity = 0.33,
+            opacity = 0.8,
             hsb = {
                 brightness = 0.1
             }
@@ -44,7 +53,7 @@ M = {
             action = wezterm.action.ToggleFullScreen,
         },
         {
-            key = 'h',
+            key = 'F',
             mods = 'SHIFT|CTRL',
             action = wezterm.action.ToggleFullScreen,
         },
