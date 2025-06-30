0e9a8e22e3c24afe342a33da60b9b261f0031005
Giovanni Panzetta <giovanni.panzetta@daveramsey.com>
2025-06-30 10:24:11 -0500

    dap mappings

--- nvim2024/lua/giovanni/lazy/dap.lua
+++ nvim2024/lua/giovanni/lazy/dap.lua
@@ -14,6 +14,27 @@ return {
                 }
             }
 
+            vim.keymap.set('n', '<leader>dc', function() require('dap').continue() end)
+            vim.keymap.set('n', '<leader>dl', function() require('dap').step_over() end)
+            vim.keymap.set('n', '<leader>dj', function() require('dap').step_into() end)
+            vim.keymap.set('n', '<leader>dk', function() require('dap').step_out() end)
+            vim.keymap.set('n', '<leader>db', function() require('dap').toggle_breakpoint() end)
+            vim.keymap.set('n', '<leader>dr', function() require('dap').repl.open() end)
+            vim.keymap.set({'n', 'v'}, '<leader>dh', function()
+              require('dap.ui.widgets').hover()
+            end)
+            vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
+              require('dap.ui.widgets').preview()
+            end)
+            vim.keymap.set('n', '<Leader>df', function()
+              local widgets = require('dap.ui.widgets')
+              widgets.centered_float(widgets.frames)
+            end)
+            vim.keymap.set('n', '<Leader>ds', function()
+              local widgets = require('dap.ui.widgets')
+              widgets.centered_float(widgets.scopes)
+            end)
+
         end
     },
     {
