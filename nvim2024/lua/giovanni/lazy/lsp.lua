08ace8ed59744c51c763feef2809f1169e0de927
Giovanni Panzetta <giovanni.panzetta@daveramsey.com>
2025-07-07 12:18:37 -0500

    update vue_ls to v3

--- nvim2024/lua/giovanni/lazy/lsp.lua
+++ nvim2024/lua/giovanni/lazy/lsp.lua
@@ -47,16 +47,25 @@ return {
                         function() vim.lsp.buf.execute_command({ command = "_typescript.organizeImports", arguments = { vim.fn.expand("%:p") } }) end,
                         {})
                 end,
-                init_options = {
-                    plugins = {
-                        {
-                            name = '@vue/typescript-plugin',
-                            location = vim.fn.stdpath 'data' ..
-                                '/mason/packages/vue-language-server/node_modules/@vue/language-server',
-                            languages = { 'vue' },
+            })
+
+            vim.lsp.config('vtsls', {
+                settings = {
+                    vtsls = {
+                        tsserver = {
+                            globalPlugins = {
+                                {
+                                    name = '@vue/typescript-plugin',
+                                    location = vim.fn.stdpath('data') ..
+                                    '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+                                    languages = { 'vue' },
+                                    configNamespace = 'typescript'
+                                }
+                            },
                         }
                     }
-                }
+                },
+                filetypes = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact', 'vue' }
             })
 
             vim.lsp.config('vue_ls', {
@@ -67,11 +76,6 @@ return {
                         function() vim.lsp.buf.execute_command({ command = "_typescript.organizeImports", arguments = { vim.fn.expand("%:p") } }) end,
                         {})
                 end,
-                init_options = {
-                    vue = {
-                        hybridMode = false,
-                    }
-                },
                 settings = {
                     vue = {
                         complete = {
@@ -81,7 +85,32 @@ return {
                             }
                         }
                     }
-                }
+                },
+                on_init = function(client)
+                    client.handlers['tsserver/request'] = function(_, result, context)
+                        local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = 'vtsls' })
+                        if #clients == 0 then
+                            vim.notify('Could not found `vtsls` lsp client, vue_lsp would not work without it.',
+                                vim.log.levels.ERROR)
+                            return
+                        end
+                        local ts_client = clients[1]
+
+                        local param = unpack(result)
+                        local id, command, payload = unpack(param)
+                        ts_client:exec_cmd({
+                            title = 'vue_request_forward',
+                            command = 'typescript.tsserverRequest',
+                            arguments = {
+                                command,
+                                payload,
+                            },
+                        }, { bufnr = context.bufnr }, function(_, r)
+                            local response_data = { { id, r.body } }
+                            client:notify('tsserver/response', response_data)
+                        end)
+                    end
+                end,
             })
 
             vim.lsp.config('html', {
@@ -101,6 +130,8 @@ return {
                     'ts_ls',
                     'eslint',
                     'gopls',
+                    'vue_ls',
+                    'vtsls'
                 },
             }
         end
