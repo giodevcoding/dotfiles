e914664db4466ca237f45defa0ed1f769e997d36
Giovanni Panzetta <giovanni.panzetta@daveramsey.com>
2026-03-27 10:59:15 -0500

    replace plantuml rendering

--- nvim2024/lua/giovanni/lazy/previewing.lua
+++ nvim2024/lua/giovanni/lazy/previewing.lua
@@ -9,10 +9,38 @@ return {
         end,
     },
     {
-        'weirongxu/plantuml-previewer.vim',
-        dependencies = {
-            'tyru/open-browser.vim',
-            'aklt/plantuml-syntax'
+        'javiorfo/nvim-soil',
+
+        dependencies = { 'javiorfo/nvim-nyctophilia' },
+
+        lazy = true,
+        ft = "plantuml",
+        opts = {
+            -- If you want to change default configurations
+
+            -- This option closes the image viewer and reopen the image generated
+            -- When true this offers some kind of online updating (like plantuml web server)
+            actions = {
+                redraw = false
+            },
+
+            -- If you want to use Plant UML jar version instead of the installed version
+            puml_jar = "/opt/homebrew/Cellar/plantuml/1.2026.2/libexec/plantuml.jar",
+
+            -- If you want to customize the image showed when running this plugin
+            image = {
+                darkmode = false, -- Enable or disable darkmode
+                format = "png", -- Choose between png or svg
+
+                -- This is a default implementation of using nsxiv to open the resultant image
+                -- Edit the string to use your preferred app to open the image (as if it were a command line)
+                -- Some examples:
+                -- return "feh " .. img
+                -- return "xdg-open " .. img
+                execute_to_open = function(img)
+                    return "open " .. img
+                end
+            }
         }
     }
 }
